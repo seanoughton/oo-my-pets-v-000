a1d3db4cd147940cbef2b8741256af7e2f48a8d0
Sean Oughton <seanoughton@gmail.com>
2018-02-09 23:00:46 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -62,9 +62,10 @@ class Owner
   end
 
   def sell_pets()
-    self.change_pets_mood(:dogs,"nervous")
-    self.change_pets_mood(:cats,"nervous")
-    self.change_pets_mood(:fishes,"nervous")
+    @pets
+    #self.change_pets_mood(:dogs,"nervous")
+    #self.change_pets_mood(:cats,"nervous")
+    #self.change_pets_mood(:fishes,"nervous")
 
   end
 
