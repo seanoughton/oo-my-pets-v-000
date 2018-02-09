1a7d1e3b1eda4fda3edf34002d9d867b263cd8a3
Sean Oughton <seanoughton@gmail.com>
2018-02-09 22:06:01 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,23 @@
 class Owner
-  # code goes here
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all()
+  end
+
+  def self.count()
+    self.all.length
+  end
+
 end
